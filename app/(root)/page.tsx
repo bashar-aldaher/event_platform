b4a7b84233436abe2fbc083edba00eb9f73fb554
bashar aldaher <basharaldaher011@gmail.com>
2024-06-21 00:44:09 +0300

    edit params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,13 @@ import Events from "@/components/home/events/Events";
 import Header from "@/components/home/header/Header";
 
 export default function Home() {
+  const params = { id: "1" };
+  const searchParams = { page: "1" };
+  
   return (
     <>
       <Header />
-      <Events />
+      <Events params={params} searchParams={searchParams} />
     </>
   );
 }
