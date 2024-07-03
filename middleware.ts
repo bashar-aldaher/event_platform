228919837803d3c7b7c4212d532ecd52c48a7fea
bashar aldaher <basharaldaher011@gmail.com>
2024-07-03 08:38:39 +0300

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,18 @@
-// import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// export default authMiddleware({
-//   publicRoutes: [
-//     "/",
-//     "/events/:id",
-//     "/api/webhook/clerk",
-//     "/api/webhook/stripe",
-//     "/api/uploadthing",
-//   ],
-//   ignoredRoutes: [
-//     "/api/webhook/clerk",
-//     "/api/webhook/stripe",
-//     "/api/uploadthing",
-//   ],
-// });
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/events/(.*)",
+  "/api/webhook/clerk",
+  "/api/webhook/stripe",
+  "/api/uploadthing",
+]);
 
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
